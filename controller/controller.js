d0e87712e17c02433992c247ea5e97fce8e6d7df
Manikanta <manikantaannavarapu03@gmail.com>
2024-11-27 12:44:16 +0530

    initital commit

--- controller/controller.js
+++ controller/controller.js
@@ -43,10 +43,75 @@ async function handleLogin(req, res){
         
 //.then(async () => await consumer.disconnect())
         
+    }catch(error){
+        console.log(error)
     }
     finally{
         console.log("process completed")
     }
 }
 
-module.exports = {handleLogin}
+async function handleSignup(req, res){
+    try{
+        const producer = kafka.producer({createPartitioner: Partitioners.LegacyPartitioner})
+        const body = req.body
+        await producer.connect()
+        console.log("producer connected")
+        await producer.send({
+            topic:'user-reqq-topic',
+            messages:[{
+                key:'signup',value:JSON.stringify({action:'signup', data:body})
+            }]
+        })
+        await producer.disconnect()
+        console.log("producer disconnected")
+        const consumer = kafka.consumer({groupId:'user-ress-group', autoCommit:true})
+        await consumer.connect()
+        console.log("consumer connected")
+        await consumer.subscribe({topic:'user-ress-topic'})
+        await consumer.run({
+            eachMessage: async({topic, partition, message}) =>{
+                const userMessage = JSON.parse(message.value.toString())
+                if (!res.headersSent){
+                    if(userMessage == "user created"){
+                        res.status(201).json({message:userMessage})
+                        consumer.disconnect()
+                    }
+                    else if(userMessage == "user not created"){
+                        res.status(400).json({message:userMessage})
+                        consumer.disconnect()
+                    }
+                    
+                }
+            }
+        })
+        
+//.then(async () => await consumer.disconnect())
+        
+    }
+    finally{
+        console.log("process completed")
+    }
+}
+
+async function handlePostOrderDetails(req, res){
+    try{
+    const data = req.data
+   const producer = kafka.producer({createPartitioner:Partitioners.LegacyPartitioner})
+   await producer.connect()
+   console.log("producer connected")
+   await producer.send({
+    topic:'order-req-topic',
+    messages:[{key:"post", value:{action:'post', data:JSON.stringify(data)}}]
+   })
+}catch(error){
+    console.log(error)
+}
+}
+
+
+async function handleGetOrderDetails(req, res){
+
+}
+
+module.exports = {handleLogin, handleSignup, handlePostOrderDetails, handleGetOrderDetails}
--- routs/router.js
+++ routs/router.js
@@ -1,10 +1,14 @@
 const {Router} = require('express')
-const { handleLogin} = require('../controller/controller.js')
+const { handleLogin, handleSignup, handlePostOrderDetails, handleGetOrderDetails} = require('../controller/controller.js')
 
 const router = Router()
 
-//router.post('/signup', handleSignup)
+router.post('/signup', handleSignup)
 
 router.post('/login', handleLogin)
+
+router.post('/data', handlePostOrderDetails)
+
+router.get('/data', handleGetOrderDetails)
 
 module.exports = router
